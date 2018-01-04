1ed3125639f8532bdef5ee037d3f8b91b333317b
Esteban Bisocoli <estebaanbisocoli@gmail.com>
2018-01-04 17:34:34 -0300

    primer commit

---
+++ src/components/PropTable.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <v-data-table
+      v-bind:headers="headers"
+      :items="items"
+      hide-actions
+      class="elevation-1"
+    >
+    <template slot="items" slot-scope="props">
+      <td>{{ props.item.direccion }}</td>
+      <td class="text-xs-left">{{ props.item.inquilino }}</td>
+      <td class="text-xs-left" ><v-icon @click='openDialog(props.item)'>phone</v-icon></td>
+      <td class="text-xs-left">{{ props.item.vencimiento }}</td>
+    </template>
+  </v-data-table>
+  <contacto :dialog="dialog" :propiedad="propiedadTarget"></contacto>
+  </div>
+</template>
+<script>
+  import Contacto from './Contacto'
+  export default {
+    name: 'prop-table',
+    components: {Contacto},
+    data () {
+      return {
+        dialog: 'false',
+        propiedadTarget: null,
+        headers: [
+          {
+            text: 'Direccion',
+            align: 'left',
+            sortable: true,
+            value: 'direccion'
+          },
+          { text: 'Inquilino', align:'left', value: 'inquilino'},
+          { text: 'Contacto', align:'left', value: 'contacto' },
+          { text: 'Vencimiento de Contrato', align: 'left', value: 'vencimiento' },
+        ],
+        items: [
+          {
+            value: false,
+            direccion: 'Belgrano 1266',
+            inquilino: 'Esteban Bisocoli Contrera',
+            vencimiento: '6/11/2018'
+
+          },
+        {
+            value: false,
+            direccion: 'Constitucion 484',
+            inquilino: 'Sebastian Villegas Zorita',
+            vencimiento: '08/06/2018'
+
+          }
+        ]
+      }
+    },
+    methods: {
+      openDialog(event) {
+        this.dialog = true
+        this.propiedadTarget = event
+      }
+    }
+  }
+</script>
---
+++ src/components/Propiedades.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container grid-list-md>
+      <prop-table></prop-table>
+  </v-container>
+</template>
+<script>
+import PropTable from './PropTable'
+export default {
+    name: 'propiedades',
+    components: {PropTable}
+}
+</script>
+
